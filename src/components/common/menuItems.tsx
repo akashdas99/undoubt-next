36e5d04c01bad4e35ee1600718f5d0922f62512b
Akash Das <akashdas0599@gmail.com>
2025-01-20 01:03:08 +0530

    feat: logout user

--- src/components/common/menuItems.tsx
+++ src/components/common/menuItems.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { logoutUser } from "@/actions/auth";
 import { MenuItem } from "@/lib/types";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function MenuItemComponent({ item }: { item: MenuItem }) {
+  const router = useRouter();
   return (
-    <Link href={item.href} className="link">
+    <Link
+      href={item.href}
+      className="link"
+      onClick={async () => {
+        if (item.title === "Logout") {
+          logoutUser();
+          router.refresh();
+        }
+      }}
+    >
       {item.title}
     </Link>
   );
