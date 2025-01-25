40b2f97c39c5938b54ffa8d8e1720d07164f7eb8
Akash Das <akashdas0599@gmail.com>
2025-01-25 21:29:06 +0530

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex justify-center grow flex-col max-w-xs w-4/5 self-center">
+      <h1 className="text-[--chefchaouen-blue] font-semibold text-3xl mb-5">
+        Page Not Found
+      </h1>
+      <p>Sorry, the page you’re looking for cannot be found.</p>
+      <Link
+        href="/"
+        className="text-[--chefchaouen-blue] underline font-semibold mt-2"
+      >
+        Return Home
+      </Link>
+    </div>
+  );
+}
