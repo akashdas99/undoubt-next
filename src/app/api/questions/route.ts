07ca7207c4ce36ea1e350e2a38d1e64e63e8e616
Akash Das <akashdas0599@gmail.com>
2025-03-01 20:23:16 +0530

    feat: question services

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,10 +1,10 @@
+import { searchQuestions } from "@/services/question";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const params = request?.nextUrl?.searchParams?.get("keyword");
-  const res = await fetch(
-    `${process.env.REACT_APP_BACKEND}/search/questions/?q=${params}`
-  );
-  const data = await res.json();
-  return Response.json(data);
+  const keyword = request?.nextUrl?.searchParams?.get("keyword");
+  if (!keyword) return Response.json([]);
+  const questions = await searchQuestions(keyword);
+
+  return Response.json(questions);
 }
