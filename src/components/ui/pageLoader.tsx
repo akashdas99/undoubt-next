a6687e885c00e73f3815e946a9530defd2d1f617
Akash Das <akashdas0599@gmail.com>
2025-03-01 20:39:49 +0530

    feat: loading page

---
+++ src/components/ui/pageLoader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PageLoader() {
+  return (
+    <div className="grow grid">
+      <div className="spinner-loader w-full place-self-center bg-primary"></div>
+    </div>
+  );
+}
