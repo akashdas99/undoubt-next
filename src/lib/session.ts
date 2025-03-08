647c06dc727d12b71528d7d363193c62f66fcfb7
Akash Das <akashdas0599@gmail.com>
2025-03-08 20:26:30 +0530

    feat: replaced jsonwebtoken with jose

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,19 +1,23 @@
+import { SignJWT, jwtVerify } from "jose";
 import { cookies } from "next/headers";
 import { UserType } from "./types";
-import jwt from "jsonwebtoken";
 
-export function createSession(tokenData: Partial<UserType>) {
-  const token = jwt.sign(tokenData, process.env.SECRET!, {
-    expiresIn: "10h",
-  });
+export async function createSession(tokenData: Partial<UserType>) {
+  const jwtKey = new TextEncoder().encode(process.env.SECRET!);
+  const token = await new SignJWT(tokenData)
+    .setProtectedHeader({ alg: "dir", enc: "A128CBC-HS256" })
+    .setIssuedAt()
+    .setExpirationTime("10h")
+    .sign(jwtKey);
+
   cookies().set("token", token, { httpOnly: true });
 }
-export async function getSession(): Promise<
-  (Partial<UserType> & jwt.JwtPayload) | null
-> {
+export async function getSession() {
   const token = await cookies().get("token")?.value;
   if (!token) return null;
-  return jwt.verify(token, process.env.SECRET!) as Partial<UserType>;
+  const jwtKey = new TextEncoder().encode(process.env.SECRET!);
+  const { payload } = await jwtVerify(token, jwtKey);
+  return payload as Partial<UserType>;
 }
 export async function removeSession() {
   await cookies().delete("token");
