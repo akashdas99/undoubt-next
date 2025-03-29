d9b1b6b88458c8ae85156ec5e164e2edaf413f17
Akash Das <akashdas0599@gmail.com>
2025-03-29 23:54:24 +0530

    feat: loading for question details page

--- src/app/question/[slug]/page.tsx
+++ src/app/question/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import AddAnswer from "@/components/answer/addAnswer";
 import AnswerList from "@/components/answer/answerList";
 import QuestionSection from "@/components/question/questionSection";
+import PageLoader from "@/components/ui/pageLoader";
 import { getSession } from "@/lib/session";
 import { getQuestions } from "@/services/question";
+import { Suspense } from "react";
 
 export const revalidate = 600;
 
@@ -19,14 +21,17 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <div className="px-[8vw] mt-8">
       <div className="flex flex-col gap-5">
-        <QuestionSection slug={params?.slug} />
+        <Suspense fallback={<PageLoader />}>
+          <QuestionSection slug={params?.slug} />
+        </Suspense>
         {session && <AddAnswer />}
 
         <div className="active-neo section-heading mb-2 font-righteous text-xl">
           Recent Answers
         </div>
-
-        <AnswerList slug={params?.slug} />
+        <Suspense fallback={<PageLoader />}>
+          <AnswerList slug={params?.slug} />
+        </Suspense>
       </div>
     </div>
   );
