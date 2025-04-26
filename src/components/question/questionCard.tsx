367f9d55286f0ad76a7f7ebff8fd798960165d06
Akash Das <akashdas0599@gmail.com>
2025-04-26 21:53:39 +0530

    feat: question description + text editor content component

--- src/components/question/questionCard.tsx
+++ src/components/question/questionCard.tsx
@@ -3,6 +3,7 @@ import { User } from "@/models/user";
 import dayjs from "dayjs";
 import { CalendarDays, MessageSquare } from "lucide-react";
 import UserImage from "../ui/userImage";
+import TextEditorContent from "../ui/textEditorContent";
 
 export default async function QuestionCard({
   question,
@@ -12,8 +13,8 @@ export default async function QuestionCard({
   };
 }) {
   return (
-    <div className="p-[1em] bordered-card flex flex-col gap-2">
-      <div className="flex items-center gap-2">
+    <div className="p-[1em] bordered-card flex flex-col gap-2 items-start">
+      <div className="flex items-center gap-2 pb-2 border-b-2 border-solid border-foreground/10">
         <UserImage user={question?.author} className="w-[30px]" />
         <div className="font-montserrat font-medium">
           {question?.author?.name}
@@ -23,10 +24,18 @@ export default async function QuestionCard({
           {dayjs(question?.createdAt).format("MMM D, YYYY")}
         </div>
       </div>
-      <div className="font-semibold text-sm md:text-2xl">{question?.title}</div>
-      <div className="text-xs flex gap-2">
-        <MessageSquare size={16} strokeWidth={0.5} />
-        {question?.answers?.length || "No"} Answers
+      {/* <div className="border-t-2 border-solid border-foreground/10"></div> */}
+      <div className="font-semibold text-base md:text-xl leading-tight">
+        {question?.title}
+      </div>
+      {question?.description && (
+        <TextEditorContent content={question?.description} />
+      )}
+      <div className="flex mt-1">
+        <div className="text-xs flex gap-2 bordered-card py-1 px-2 font-semibold">
+          <MessageSquare size={16} />
+          {question?.answers?.length || "No"} Answers
+        </div>
       </div>
     </div>
   );
