b45aea1cba6cf3241694290a39916617f7a237c1
Akash Das <akashdas0599@gmail.com>
2025-05-05 00:03:39 +0530

    feat: question section skeleton

--- src/app/question/[slug]/page.tsx
+++ src/app/question/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import AddAnswer from "@/components/answer/addAnswer";
 import AnswerList from "@/components/answer/answerList";
+import QuestionCard from "@/components/question/questionCard";
 import QuestionSection from "@/components/question/questionSection";
 import { getSession } from "@/lib/session";
 import { getQuestions } from "@/services/question";
+import { Suspense } from "react";
 
 export async function generateStaticParams() {
   const questions = await getQuestions();
@@ -14,13 +16,17 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <div className="w-full my-3 md:my-8 max-w-screen-lg px-3">
       <div className="flex flex-col gap-5">
-        <QuestionSection slug={params?.slug} />
+        <Suspense fallback={<QuestionCard isLoading={true} />}>
+          <QuestionSection slug={params?.slug} />
+        </Suspense>
         {session && <AddAnswer />}
         <div className="bordered-card p-[1em]">
           <div className="active-neo section-heading mb-2 font-righteous text-xl">
             Recent Answers
           </div>
-          <AnswerList slug={params?.slug} />
+          <Suspense fallback={<p>Loading...</p>}>
+            <AnswerList slug={params?.slug} />
+          </Suspense>
         </div>
       </div>
     </div>
--- src/components/question/questionCard.tsx
+++ src/components/question/questionCard.tsx
@@ -4,37 +4,71 @@ import dayjs from "dayjs";
 import { CalendarDays, MessageSquare } from "lucide-react";
 import UserImage from "../ui/userImage";
 import TextEditorContent from "../ui/textEditorContent";
+import { Skeleton } from "../ui/skeleton";
 
 export default async function QuestionCard({
   question,
+  isLoading = false,
 }: {
-  question: Omit<Question, "author"> & {
+  question?: Omit<Question, "author"> & {
     author: User;
   };
+  isLoading?: boolean;
 }) {
   return (
     <div className="p-[1em] bordered-card flex flex-col gap-2 items-start">
       <div className="flex items-center gap-2 pb-2 border-b-2 border-solid border-foreground/10">
-        <UserImage user={question?.author} className="w-[30px]" />
-        <div className="font-montserrat font-medium">
-          {question?.author?.name}
-        </div>
-        <div className="flex items-center gap-1 text-xs opacity-50">
-          <CalendarDays className="w-3" />
-          {dayjs(question?.createdAt).format("MMM D, YYYY")}
-        </div>
+        <UserImage
+          user={question?.author}
+          className="w-[30px]"
+          isLoading={isLoading}
+        />
+        {isLoading ? (
+          <Skeleton className="h-6 w-28" />
+        ) : (
+          <div className="font-montserrat font-medium">
+            {question?.author?.name}
+          </div>
+        )}
+        {isLoading ? (
+          <Skeleton className="h-6 w-20" />
+        ) : (
+          <div className="flex items-center gap-1 text-xs opacity-50">
+            <CalendarDays className="w-3" />
+            {dayjs(question?.createdAt).format("MMM D, YYYY")}
+          </div>
+        )}
       </div>
-      {/* <div className="border-t-2 border-solid border-foreground/10"></div> */}
-      <div className="font-semibold text-base md:text-xl leading-tight">
-        {question?.title}
-      </div>
-      {question?.description && (
-        <TextEditorContent content={question?.description} />
+
+      {isLoading ? (
+        <Skeleton className="h-7 w-2/3" />
+      ) : (
+        <div className="font-semibold text-base md:text-xl leading-tight">
+          {question?.title}
+        </div>
       )}
+      {isLoading ? (
+        <>
+          <Skeleton className="h-6 w-full rounded-xl" />
+          <Skeleton className="h-6 w-full rounded-xl" />
+          <Skeleton className="h-6 w-full rounded-xl" />
+        </>
+      ) : (
+        <>
+          {question?.description && (
+            <TextEditorContent content={question?.description} />
+          )}
+        </>
+      )}
+
       <div className="flex mt-1">
         <div className="text-xs flex gap-2 bordered-card py-1 px-2 font-semibold">
           <MessageSquare size={16} />
-          {question?.answers?.length || "No"} Answers
+          {isLoading ? (
+            <Skeleton className="h-[15px] w-[80px]" />
+          ) : (
+            <>{question?.answers?.length || "No"} Answers</>
+          )}
         </div>
       </div>
     </div>
