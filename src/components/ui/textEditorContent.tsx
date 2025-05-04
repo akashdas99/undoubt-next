444397785070e63e7bb65331adc5e94fdb52f982
Akash Das <akashdas0599@gmail.com>
2025-05-04 18:26:41 +0530

    feat: html parser styling

--- src/components/ui/textEditorContent.tsx
+++ src/components/ui/textEditorContent.tsx
@@ -1,8 +1,26 @@
 "use client";
-import parse from "html-react-parser";
+import parse, {
+  DOMNode,
+  domToReact,
+  HTMLReactParserOptions,
+  Element,
+} from "html-react-parser";
 
 const TextEditorContent = ({ content }: { content: string }) => {
-  return parse(content);
+  const options: HTMLReactParserOptions = {
+    replace: (domNode: DOMNode) => {
+      if (domNode instanceof Element) {
+        if (domNode?.attribs && domNode?.name === "ol") {
+          return (
+            <ol className="list-decimal ml-5">
+              {domToReact(domNode.children as DOMNode[])}
+            </ol>
+          );
+        }
+      }
+    },
+  };
+  return <div>{parse(content, options)}</div>;
 };
 
 export default TextEditorContent;
