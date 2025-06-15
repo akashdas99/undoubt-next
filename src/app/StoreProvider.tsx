76a6a5579bd7bdd8d3b39d0650cca16d4d2d1af3
Akash Das <akashdas0599@gmail.com>
2025-06-15 22:18:37 +0530

    fix: useRef initial data

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -4,7 +4,7 @@ import { ReactNode, useRef } from "react";
 import { Provider } from "react-redux";
 
 const StoreProvider = ({ children }: { children: ReactNode }) => {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore>(null);
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
