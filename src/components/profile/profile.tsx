ac6aff5493dff25259fb88fb4b8a8c0a2d6d8387
Akash Das <akashdas0599@gmail.com>
2025-06-22 21:40:14 +0530

    fix: max width of profile and add answer

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -8,13 +8,13 @@ export default async function Profile() {
 
   return (
     <div className="flex items-center justify-center grow">
-      <div className="bordered-card p-5 rounded-xl max-w-sm w-4/5">
+      <div className="bordered-card p-5 rounded-xl max-w-screen-lg">
         <h1 className={`font-righteous text-xl md:text-3xl mb-6`}>
           Profile Information
         </h1>
         <div className="flex flex-col gap-5 sm:flex-row sm:justify-between items-start">
           <div className="relative">
-            <UserImage user={data} className="w-[26px]" />
+            <UserImage user={data} className="w-[36px]" />
             <ImageUpload />
           </div>
           <div className="grid grid-cols-[min-content_auto] text-sm gap-y-2 gap-x-2 flex-1">
